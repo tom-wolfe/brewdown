1e0212f1f5f1741cff426d65f762f2fe5339610d
Tom Wolfe <trwolfe13@gmail.com>
2017-08-02 00:18:23 +0100

    Add beginnings of abilityScore rule.

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,21 @@
+const abilityScores = require('./rules/abilityScores')
 const columnBreak = require('./rules/columnBreak')
 const lists = require('./rules/lists')
 const pageBreak = require('./rules/pageBreak')
 const statBlock = require('./rules/statBlock')
 const textBlock = require('./rules/textBlock')
 
-module.exports = (md) => {
-  md.use(pageBreak)
-  md.use(columnBreak)
-  md.use(textBlock)
-  md.use(statBlock)
-  md.use(lists)
+module.exports = (md, options) => {
+  md.use(pageBreak, options)
+  md.use(columnBreak, options)
+  md.use(textBlock, options)
+  md.use(statBlock, options)
+  md.use(abilityScores, options)
+  md.use(lists, options)
 }
+
+const fs = require('fs-extra')
+const MarkdownIt = require('markdown-it')
+const md = MarkdownIt().use(module.exports)
+const file = fs.readFileSync('src/examples/ability-table.md').toString()
+console.log(md.render(file))
---
+++ src/js/rules/abilityScores.js
@@ -0,0 +1,46 @@
+module.exports = function (md, options) {
+  const openMarker = '{abilities:'
+  const closeMarker = '}'
+  const pattern = /([a-z]+)\s*=\s*(\d+)/gi
+  const rule = state => {
+    const blockTokens = state.tokens
+
+    for (let j = 0, l = blockTokens.length; j < l; j++) {
+      const blockToken = blockTokens[j]
+
+      if (blockToken.type !== 'inline' || blockToken.content.indexOf(openMarker) < 0) {
+        continue
+      }
+
+      let tokens = blockToken.children
+      for (let i = tokens.length - 1; i >= 0; i--) {
+        const token = tokens[i]
+        const content = token.content
+        const openIndex = content.indexOf(openMarker)
+        const closeIndex = content.indexOf(closeMarker)
+        if (token.type === 'text' && openIndex > -1 && closeIndex > -1) {
+          const newTokens = []
+
+          const preToken = new state.Token('text', '', token.level)
+          preToken.content = content.substring(0, openIndex)
+          newTokens.push(preToken)
+
+          const pairs = {}
+          let match
+          while ((match = pattern.exec(content))) {
+            pairs[match[1].toLowerCase()] = match[2]
+          }
+
+          // TODO: Inject ability scores.
+
+          const postToken = new state.Token('text', '', token.level)
+          postToken.content = content.substring(closeIndex + 1)
+          newTokens.push(postToken)
+
+          blockToken.children = tokens = state.md.utils.arrayReplaceAt(tokens, i, newTokens)
+        }
+      }
+    }
+  }
+  md.core.ruler.push('ability_scores', rule)
+}
