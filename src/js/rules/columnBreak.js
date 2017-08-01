b725a047b2cf5fc9a9fa36eea68663c8e2602c99
Tom Wolfe <trwolfe13@gmail.com>
2017-08-01 09:23:53 +0100

    Add styleless lists.

--- src/js/rules/columnBreak.js
+++ src/js/rules/columnBreak.js
@@ -1,63 +1,64 @@
-const NAME = "column_break";
+module.exports = function (md) {
 
-const rule = state => {
-  const blockTokens = state.tokens;
+  const NAME = "column_break";
 
-  for (let j = 0, l = blockTokens.length; j < l; j++) {
-    if (blockTokens[j].type !== "inline" || blockTokens[j].content.indexOf("/column") < 0) {
-      continue;
-    }
+  function rule(state) {
+    const blockTokens = state.tokens;
 
-    let htmlLinkLevel = 0;
-    let tokens = blockTokens[j].children;
-
-    // We scan from the end, to keep position when new tags added.
-    // Use reversed logic in links start/end match
-    for (let i = tokens.length - 1; i >= 0; i--) {
-      const currentToken = tokens[i];
-
-      // Skip content of markdown links
-      if (currentToken.type === "link_close") {
-        i--;
-        while (tokens[i].level !== currentToken.level && tokens[i].type !== "link_open") { i--; }
+    for (let j = 0, l = blockTokens.length; j < l; j++) {
+      if (blockTokens[j].type !== "inline" || blockTokens[j].content.indexOf("/column") < 0) {
         continue;
       }
 
-      // Skip content of html tag links
-      if (currentToken.type === "html_inline") {
-        if (state.md.utils.isLinkOpen(currentToken.content) && htmlLinkLevel > 0) { htmlLinkLevel--; }
-        if (state.md.utils.isLinkClose(currentToken.content)) { htmlLinkLevel++; }
-      }
+      let htmlLinkLevel = 0;
+      let tokens = blockTokens[j].children;
 
-      if (htmlLinkLevel > 0) { continue; }
+      // We scan from the end, to keep position when new tags added.
+      // Use reversed logic in links start/end match
+      for (let i = tokens.length - 1; i >= 0; i--) {
+        const currentToken = tokens[i];
 
-      if (currentToken.type === "text" && currentToken.content.indexOf("/column") > -1) {
-        const nodes = [];
-        const level = currentToken.level;
-        const parts = currentToken.content.split("/column");
-        parts.forEach((part, idx) => {
-          if (part) {
-            const token = new state.Token("text", "", 0);
-            token.content = part;
-            token.level = level;
-            nodes.push(token);
-          }
-          if (idx < parts.length - 1) {
-            const columnBreak = new state.Token(NAME, "", 0);
-            columnBreak.content = "/column";
-            columnBreak.level = level;
-            nodes.push(columnBreak);
-          }
-        });
+        // Skip content of markdown links
+        if (currentToken.type === "link_close") {
+          i--;
+          while (tokens[i].level !== currentToken.level && tokens[i].type !== "link_open") { i--; }
+          continue;
+        }
 
-        // replace current node
-        blockTokens[j].children = tokens = state.md.utils.arrayReplaceAt(tokens, i, nodes);
+        // Skip content of html tag links
+        if (currentToken.type === "html_inline") {
+          if (state.md.utils.isLinkOpen(currentToken.content) && htmlLinkLevel > 0) { htmlLinkLevel--; }
+          if (state.md.utils.isLinkClose(currentToken.content)) { htmlLinkLevel++; }
+        }
+
+        if (htmlLinkLevel > 0) { continue; }
+
+        if (currentToken.type === "text" && currentToken.content.indexOf("/column") > -1) {
+          const nodes = [];
+          const level = currentToken.level;
+          const parts = currentToken.content.split("/column");
+          parts.forEach((part, idx) => {
+            if (part) {
+              const token = new state.Token("text", "", 0);
+              token.content = part;
+              token.level = level;
+              nodes.push(token);
+            }
+            if (idx < parts.length - 1) {
+              const columnBreak = new state.Token(NAME, "", 0);
+              columnBreak.content = "/column";
+              columnBreak.level = level;
+              nodes.push(columnBreak);
+            }
+          });
+
+          // replace current node
+          blockTokens[j].children = tokens = state.md.utils.arrayReplaceAt(tokens, i, nodes);
+        }
       }
     }
-  }
-};
+  };
 
-module.exports = function (md) {
   md.core.ruler.push(NAME, rule);
-  md.renderer.rules[NAME] = () => "\n<i class=\"col-break\"></i>\n";
+  md.renderer.rules[NAME] = () => "\n<div class=\"col-break\"></div>\n";
 };
--- src/js/rules/lists.js
+++ src/js/rules/lists.js
@@ -1,4 +1,18 @@
 module.exports = function (md) {
-  // TODO: Asterisks give list styles, dashes do not.
-  console.log(md);
+
+  const rule = state => {
+    const blockTokens = state.tokens;
+
+    for (let i = 0; i < state.tokens.length; i++) {
+      const token = blockTokens[i];
+      if (token.type !== "bullet_list_open") {
+        continue;
+      }
+      if (token.markup === "-") {
+        token.attrPush(["class", "no-bullet"]);
+      }
+    }
+  };
+
+  md.core.ruler.push("list_style", rule);
 };
