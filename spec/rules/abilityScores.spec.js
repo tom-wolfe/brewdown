5f125b1d495acf936a8f2872a626fe7a2b507ad3
Tom Wolfe <trwolfe13@gmail.com>
2017-08-03 08:34:35 +0100

    Remove testing stuff.

--- spec/rules/abilityScores.spec.js
+++ spec/rules/abilityScores.spec.js
@@ -1,12 +1,8 @@
-const MarkdownIt = require('markdown-it')
-const abilityScores = require('../../src/js/rules/abilityScores')
+// const MarkdownIt = require('markdown-it')
+// const abilityScores = require('../../src/js/rules/abilityScores')
 
 describe('abilityScores', () => {
   describe('render', () => {
-    it('should produce the right output with default settings', () => {
-      const md = MarkdownIt().use(abilityScores)
-      const html = md.render('{abilities: str=10 dex=10 con=10 wis=10 int=10 cha=10}')
-      expect(html).toEqual('\n<div class="col-break"></div>\n')
-    })
+    // TODO: Implement ability score tests.
   })
 })
---
+++ src/examples/ability-scores.md
@@ -0,0 +1,1 @@
+{abilities: STR=15, DEX=13, CON=1, INT=13, WIS=1, CHA=9}
